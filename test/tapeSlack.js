9b3101ff90d037f40ef6819c8755810325340dc1
jaunin-b <jaunin.b@fr.ibm.com>
2016-01-19 18:17:45 +0100

    Task 52298 : Support lazy config of slack config (ie PATCH support)

--- test/tapeSlack.js
+++ test/tapeSlack.js
@@ -36,7 +36,7 @@ var defaultHeaders = {
     'Content-Type': 'application/json'
 };
 
-var mockServiceInstanceId = "1234";
+var mockServiceInstanceId = "tape" + new Date().getTime();
 var mockToolchainId = "c234adsf-111";
 
 var header = {};
@@ -120,7 +120,7 @@ test('Slack Broker - Test PUT instance', function (t) {
                     
                     body.parameters = {
                     	api_token: nconf.get("slack-token"),
-                    	channel_name: slack_channel.name,
+                    	channel_name: slack_channel.name.replace("bot", "bis"),
                     	channel_topic: slack_channel.topic
                     }
                     
@@ -132,7 +132,7 @@ test('Slack Broker - Test PUT instance', function (t) {
                             t.ok(results.body.instance_id, 'did the put instance call return an instance_id?');
                             slack_channel.id = results.body.instance_id;
                             
-                            //t.comment(slack_channel.id);
+                            //t.comment("channel.id is " + slack_channel.id);
                             
                             // Ensure Slack Channel has been created
                             slack.api("channels.info", {channel: slack_channel.id}, function(err, response) {
@@ -150,7 +150,7 @@ test('Slack Broker - Test PUT instance', function (t) {
 });
 
 test('Slack Broker - Test PUT update instance w/o parameters', function (t) {
-    t.plan(2);
+    t.plan(1);
 
     var body = {
         'service_id': 'slack',
@@ -161,9 +161,41 @@ test('Slack Broker - Test PUT update instance w/o parameters', function (t) {
     var url = nconf.get('url') + '/slack-broker/api/v1/service_instances/' + mockServiceInstanceId;
     putRequest(url, {header: header, body: JSON.stringify(body)})
         .then(function(resultFromPut) {
-            t.equal(resultFromPut.statusCode, 200, 'did the put instance call succeed?');
-            t.ok(resultFromPut.body.instance_id, 'did the put instance call return an instance_id?');
+            t.equal(resultFromPut.statusCode, 400, 'did the put instance call failed?');
     });
+});
+
+test('Slack Broker - Test PATCH update instance with channel_name', function (t) {
+    t.plan(3);
+	
+    var body = {
+        'service_id': 'slack',
+        'organization_guid': nconf.get('test_app_org_guid'),
+        'parameters' : {
+        	channel_name: slack_channel.name        	
+        }
+    };
+
+    var url = nconf.get('url') + '/slack-broker/api/v1/service_instances/' + mockServiceInstanceId;
+    patchRequest(url, {header: header, body: JSON.stringify(body)})
+        .then(function(resultFromPatch) {
+        	//t.comment(JSON.stringify(resultFromPatch));
+            t.equal(resultFromPatch.statusCode, 200, 'did the patch instance call succeed?');
+            //t.comment(JSON.stringify(slack_channel));
+            t.notEqual(resultFromPatch.body.instance_id, slack_channel.id, 'did the put instance call return the appropriate channel id?');
+            slack_channel.id_bis = slack_channel.id; 
+            slack_channel.id = resultFromPatch.body.instance_id;
+            // Ensure Slack Channel has been created
+            slack.api("channels.info", {channel: slack_channel.id}, function(err, response) {
+            	if (err) {
+            		t.end(err);
+            	} else if (!response.ok) {
+            		t.fail(response.error);
+            	} else {
+                	t.ok(response.ok, 'did the slack channel got created appropriately?')                            		
+            	}
+            });                            
+    });    				
 });
 
 test('Slack Broker - Test PUT update instance with channel_id (archived channel)', function (t) {
@@ -228,6 +260,7 @@ test('Slack Broker - Test PUT update instance w/ an invalid org id', function (t
     });
 });
 
+
 test('Slack Broker - Test PUT bind instance to toolchain', function (t) {
     t.plan(1);
 
@@ -250,6 +283,7 @@ test('Slack Broker - Test PUT bind instance to toolchain w/ other org', function
             t.equal(resultsFromBind.statusCode, 403, 'did the instance with other org fail to bind to toolchain?');
     });
 });
+
 
 test('Slack Broker - Test DELETE instance w/ other org', function (t) {
     t.plan(1);
@@ -274,13 +308,18 @@ test('Slack Broker - Test DELETE instance', function (t) {
     });
 });
 
+
 // Unbind tests, the service instance will still remain in the DB
 test('Slack Broker - Test DELETE unbind instance from toolchain w/ other org', function (t) {
     t.plan(5);
 
     var body = {
         'service_id': 'slack',
-        'organization_guid': nconf.get('test_app_org_guid')
+        'organization_guid': nconf.get('test_app_org_guid'),
+        'parameters' : {
+        	api_token: nconf.get("slack-token"),
+        	channel_id: slack_channel.id        	
+        }
     };
     var auth = {
         'Authorization': authenticationTokens[1]
@@ -314,7 +353,11 @@ test('Slack Broker - Test DELETE unbind instance from toolchain', function (t) {
 
     var body = {
         'service_id': 'slack',
-        'organization_guid': nconf.get('test_app_org_guid')
+        'organization_guid': nconf.get('test_app_org_guid'),
+        'parameters' : {
+        	api_token: nconf.get("slack-token"),
+        	channel_id: slack_channel.id        	
+        }
     };
 
     var url = nconf.get('url') + '/slack-broker/api/v1/service_instances/' + mockServiceInstanceId;
@@ -341,8 +384,18 @@ test('Slack Broker - Test DELETE unbind instance from toolchain', function (t) {
 });
 
 test('Slack Broker - Archive Test Slack Channel', function(t) {
-	t.plan(1);
+	// This is only to have a kind of cleanup
+	t.plan(2);
     slack.api("channels.archive", {channel: slack_channel.id}, function(err, response) {
+    	if (err) {
+    		t.end(err);
+    	} else if (!response.ok) {
+    		t.fail(response.error);
+    	} else {
+        	t.ok(response.ok, 'did the slack channel get archived for deletion?');
+    	}
+    });                            
+    slack.api("channels.archive", {channel: slack_channel.id_bis}, function(err, response) {
     	if (err) {
     		t.end(err);
     	} else if (!response.ok) {
@@ -450,6 +503,25 @@ function putRequest(url, options) {
 
     var put = Q.nbind(request.put, this);
     return put(params.uri, {body: params.body, headers: params.headers})
+        .then(function(res) {
+            if(res[1]) {
+                   return {
+                    "statusCode": res[0].statusCode,
+                    "body": JSON.parse(res[1])
+                };
+            } else {
+                return {
+                    "statusCode": res[0].statusCode
+                };
+            }
+        });
+}
+
+function patchRequest(url, options) {
+    var params = initializeRequestParams(url, options);
+
+    var patch = Q.nbind(request.patch, this);
+    return patch(params.uri, {body: params.body, headers: params.headers})
         .then(function(res) {
             if(res[1]) {
                    return {
